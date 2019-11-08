74ce50ba56020d4a193afd32de6bc712a7599b14
rickywychoi <wchoi40@my.bcit.ca>
2019-11-08 14:19:24 -0800

    directory arranged

---
+++ scripts/Post/post.js
@@ -0,0 +1,27 @@
+console.log("[post]");
+
+var user = firebase.auth().currentUser;
+var name, email, photoUrl, uid, emailVerified;
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    if (user != null) {
+      name = user.displayName;
+      email = user.email;
+      photoUrl = user.photoURL;
+      emailVerified = user.emailVerified;
+      uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
+      // this value to authenticate with your backend server, if
+      // you have one. Use User.getToken() instead.
+    }
+
+    console.log(name);
+    document.getElementById("userName").innerHTML = "Hello, " + name;
+    document.getElementById("sidebarLogIn").style.display = "none";
+
+  } else {
+    // No user is signed in.
+    document.getElementById("sidebarLogOut").style.display = "none";
+  }
+});
---
+++ scripts/firebaseConfig.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAv-mg6O3jfsV6MBeJdXCB4zqevWrmRkVc",
+  authDomain: "bcit-buy-n-sell.firebaseapp.com",
+  databaseURL: "https://bcit-buy-n-sell.firebaseio.com",
+  projectId: "bcit-buy-n-sell",
+  storageBucket: "bcit-buy-n-sell.appspot.com",
+  messagingSenderId: "796480005879",
+  appId: "1:796480005879:web:bb2942893b85e9949555f2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
