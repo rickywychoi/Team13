86e501875c6f691970a36bead6b873ffd6186a8c
rickywychoi <wchoi40@my.bcit.ca>
2019-11-14 12:47:07 -0800

    Post database connect

--- scripts/Post/post.js
+++ scripts/Post/post.js
@@ -2,6 +2,8 @@ console.log("[post]");
 
 var user = firebase.auth().currentUser;
 var name, email, photoUrl, uid, emailVerified;
+const db = firebase.firestore();
+let conditionStatus, contents, createdDate, image, postTitle, postedBy;
 
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
@@ -19,6 +21,23 @@ firebase.auth().onAuthStateChanged(function (user) {
     console.log(name);
     document.getElementById("userName").innerHTML = "Hello, " + name;
     document.getElementById("sidebarLogIn").style.display = "none";
+
+    db.collection('posts').doc('2').onSnapshot(snap => {
+      console.log(snap.data());
+      post = {
+        conditionStatus: snap.data().conditionStatus,
+        contents: snap.data().contents,
+        createdDate: snap.data().createdDate,
+        image: snap.data().image,
+        postTitle: snap.data().postTitle,
+        postedBy: snap.data().postedBy,
+      }
+      document.getElementById('postTitle').innerHTML = post.postTitle;
+      document.getElementById('user').innerHTML = post.postedBy;
+      document.getElementById('postTime').innerHTML = post.createdDate;
+      document.getElementById('postContent').innerHTML = post.contents;
+      document.getElementById('condition').innerHTML = post.conditionStatus;
+    })
 
   } else {
     // No user is signed in.
