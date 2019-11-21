b4e6c67dadd2598a36912af17395e38628a816c6
rickywychoi <wchoi40@my.bcit.ca>
2019-11-20 20:53:24 -0800

    mainHome and post

--- scripts/MainHome/mainHome.js
+++ scripts/MainHome/mainHome.js
@@ -3,6 +3,11 @@ console.log("[mainHome]");
 var user = firebase.auth().currentUser;
 var name, email, photoUrl, uid, emailVerified;
 
+// for firebase database
+const db = firebase.firestore();
+
+let postsRef = db.collection('posts');
+
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     // User is signed in.
@@ -16,9 +21,63 @@ firebase.auth().onAuthStateChanged(function (user) {
       // you have one. Use User.getToken() instead.
     }
 
+    // for sidebar
     console.log(name);
     document.getElementById("userName").innerHTML = "Hello, " + name;
     document.getElementById("sidebarLogIn").style.display = "none";
+
+    // retrieving data from each post
+    postsRef.get()
+    .then(snap => {
+      console.log("MainHome.js")
+      snap.forEach(doc => {
+        console.log(doc.data());
+
+        // fields of each post documents
+        let post = {
+          conditionStatus: doc.data().conditionStatus,
+          contents: doc.data().contents,
+          // createdDate: null || undefined ? '' : doc.data().createdDate.toDate(),
+          createdDate: doc.data().createdDate.toDate(),
+          image: doc.data().image,
+          postTitle: doc.data().postTitle,
+          postedBy: doc.data().postedBy,
+        }
+
+        // creating html elements with each post's data
+        let postDiv = $('<div></div>');
+        postDiv.addClass('post');
+        let titlePara = $('<p>' + post.postTitle + '</p>');
+        titlePara.addClass('postTitle');
+        let postLink = $('<a></a>');
+        postLink.addClass('toPost');
+        postLink.attr('href', '#');
+        let postImage = $('<div>image</div>');
+        postImage.addClass('postImage');
+        let details = $('<div></div>');
+        details.addClass('postDetail');
+        let postIdDiv = $('<div></div>');
+        postIdDiv.addClass('postId');
+        let posterLink = $('<a></a>');
+        posterLink.addClass('toPostedBy');
+        posterLink.attr('href', '#');
+        let posterPara = $('<p>' + post.postedBy + '</p>');
+        posterPara.addClass('postedBy');
+        let datePara = $('<p>' + post.createdDate + '</p>');
+        datePara.addClass('postedDate');
+        let contentsPara = $('<p>' + post.contents + '</p>');
+        contentsPara.addClass('contents');
+
+        // html elements structure
+        $('#main').append(postDiv);
+        postDiv.append(titlePara, postLink, details);
+        postLink.append(postImage);
+        posterLink.append(posterPara);
+        postIdDiv.append(posterLink, datePara);
+        details.append(postIdDiv, contentsPara);
+
+      })
+    }).catch();
 
   } else {
     // No user is signed in.
